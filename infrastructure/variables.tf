d1d9b443d7b612e2abd17ea95fef7572eb6e0bcd
SameerBramhecha14 <bramhechas89@gmail.com>
2024-10-12 14:39:45 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -21,7 +21,7 @@ variable "client_id" {
 variable "resource_group_name" {
   description = "Name of the Azure Resource Group"
   type        = string
-  default     = "capstonerg"
+  default     = "capstonerg1"
 }
 
 variable "location" {
@@ -33,18 +33,13 @@ variable "location" {
 variable "acr_name" {
   description = "Name of the Azure Container Registry"
   type        = string
-  default     = "capstoneacrsamb" # Must be globally unique
+  default     = "capstoneacrsamb1" # Must be globally unique
 }
 
 variable "aks_name" {
   description = "Name of the Azure Kubernetes Service Cluster"
   type        = string
-  default     = "capstoneaksclusameerb"
+  default     = "capstoneaksclusameerb1"
 }
 
-variable "law_name" {
-  description = "Name of the Log Analytics Workspace"
-  type        = string
-  default     = "myAppLAW"
-}
 
